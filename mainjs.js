dd902181107d902927fab4a9b3dc98f9cd92cd85
DESKTOP-B8HHBMV\Administrator <udidcs@naver.com>
2024-01-23 09:28:16 +0900

    Etc 전체적인 파일 구조부분 수정
    html에서 불필요한 html 삭제
    css 이름만 일부 변경
    js 구조 개선 및 주석 추가

--- mainjs.js
+++ mainjs.js
@@ -1,21 +1,30 @@
+// 함수 아닌 바로 실행되는 스크립트들
 const imageList = document.querySelector(".section_middle_items");
+// showmore 버튼이 클릭되면 무한스크롤 진행하기
 document.querySelector(".section_middle_items_showmore").addEventListener('click', scroll);
+// showmore 버튼이 클릭되면 하단 부분 사라지게 하기
 document.querySelector(".section_middle_items_showmore").addEventListener('click', setting);
+// 구독 버튼 누르면 모달창 띄우면서 배경 불투명하게 만들기
 document.querySelector(".footer_top_blogpost_input_subscribe").addEventListener('click', ()=>{
     document.querySelector("header").style.opacity= "0.15";
     document.querySelector("section").style.opacity= "0.15";
     document.querySelector("footer").style.opacity= "0.15";
     document.querySelector(".modal").style.display= "flex";
 });
+// okilovehodu 버튼 누르면 모달창 닫고 주변 배경 원래대로
 document.querySelector(".modal_okilovehodu").addEventListener('click', ()=>{
     document.querySelector("header").style.opacity= "1";
     document.querySelector("section").style.opacity= "1";
     document.querySelector("footer").style.opacity= "1";
     document.querySelector(".modal").style.display= "none";
 });
+
+// picsum에서 가져올 페이지 번호
 let pageToFetch = 1;
+// 무한스크롤 구현시 필요한 timer
 let timer = null;
 
+// showmore 버튼이 클릭되면 하단 부분 사라지게 하는 함수
 function setting() {
     
     document.querySelector(".section_middle_bottom").style.display= "none";
@@ -23,12 +32,14 @@ function setting() {
     document.querySelector(".section_map").style.display= "none";
     document.querySelector(".footer_top").style.display= "none";
     
+    // 제일 하단 부분 고정되게 하기
     const fb = document.querySelector(".footer_bottom");
     fb.style.position = "fixed";
     fb.style.bottom = "0px";
     fb.style.zindex = "998";
     fb.style.background = "white";
 
+    // 제일 하단 부분에 해당하는 영역의 윗 마진을 주기 위해 삽입된 margindiv 보이게 하기
     document.querySelector(".margindiv").style.display= "block";
     
 }
@@ -71,18 +82,18 @@ function makeImageList(datas){
     let cnt = 0;    
     plus = "";
     datas.forEach((item)=>{
-        
         cnt += 1;
+        // 한줄에 3개씩 보이게 하기 위해 %3
+        // class 태그 넣어 주기 위해 시작태그 삽입
         if (cnt % 3 == 1) {
             plus += "<div class='section_middle_moreitems_row'>";
         }
         plus += "<img src="+ item.download_url +" alt=''>";
+        // class 태그 넣어 주기 위해 닫는태그 삽입
         if (cnt % 3 == 0) {
             plus += "</div>";
             imageList.innerHTML += plus;
             plus = "";
         }
-        
-        
     }); 
 }
--- mapjs.js
+++ mapjs.js
@@ -4,5 +4,18 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         level: 3 // 지도의 확대 레벨
     };
 
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-var map = new kakao.maps.Map(mapContainer, mapOption);
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+// marker.setMap(null);    
